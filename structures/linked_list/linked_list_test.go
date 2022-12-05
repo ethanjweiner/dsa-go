16ac930df26f3eb1c844bd2f2d281dc297a48cb5
Ethan Weiner <ethanweiner20@gmail.com>
2022-12-05 15:32:19 -0500

    test(structures): linked list

---
+++ structures/linked_list/linked_list_test.go
@@ -0,0 +1,75 @@
+package structures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Set up suite & suite fields
+type LinkedListTestSuite struct {
+	suite.Suite
+	EmptyList    *LinkedList
+	NonEmptyList *LinkedList
+	Node1        *Node
+	Node2        *Node
+	Node3        *Node
+}
+
+// Initialize fields for setup
+func (suite *LinkedListTestSuite) SetupTest() {
+	suite.EmptyList = &LinkedList{}
+	suite.Node1 = &Node{Data: 1}
+	suite.Node2 = &Node{Data: 2}
+	suite.Node3 = &Node{Data: 3}
+	suite.NonEmptyList = &LinkedList{Head: suite.Node1}
+	suite.NonEmptyList.Prepend(suite.Node2)
+	suite.NonEmptyList.Prepend(suite.Node3)
+}
+
+// Write tests as methods of the suite
+func (suite *LinkedListTestSuite) TestPrepend() {
+	suite.EmptyList.Prepend(suite.Node1)
+	suite.Equal(suite.EmptyList.Head, suite.Node1)
+
+	suite.Equal(suite.NonEmptyList.Head, suite.Node3)
+	suite.Equal(suite.NonEmptyList.Head.Next, suite.Node2)
+}
+
+func (suite *LinkedListTestSuite) TestDeleteWithValueHead() {
+	initialLength := suite.NonEmptyList.Length
+	suite.NonEmptyList.DeleteWithValue(3)
+	suite.Equal(suite.NonEmptyList.Length, initialLength-1)
+	suite.Equal(suite.NonEmptyList.Head, suite.Node2)
+}
+
+func (suite *LinkedListTestSuite) TestDeleteWithValueMiddle() {
+	initialLength := suite.NonEmptyList.Length
+	suite.NonEmptyList.DeleteWithValue(2)
+	suite.Equal(suite.NonEmptyList.Length, initialLength-1)
+	suite.Equal(suite.NonEmptyList.Head, suite.Node3)
+}
+
+func (suite *LinkedListTestSuite) TestDeleteWithValueTail() {
+	initialLength := suite.NonEmptyList.Length
+	suite.NonEmptyList.DeleteWithValue(1)
+	suite.Equal(suite.NonEmptyList.Length, initialLength-1)
+	suite.Equal(suite.NonEmptyList.Head, suite.Node3)
+	suite.Nil(suite.NonEmptyList.Head.Next.Next)
+}
+
+func (suite *LinkedListTestSuite) TestDeleteWithValueFromEmpty() {
+	suite.EmptyList.DeleteWithValue(1)
+	suite.Equal(suite.EmptyList.Length, 0)
+}
+
+func (suite *LinkedListTestSuite) TestDeleteNonExistentValue() {
+	initialLength := suite.NonEmptyList.Length
+	suite.NonEmptyList.DeleteWithValue(5)
+	suite.Equal(suite.NonEmptyList.Length, initialLength)
+}
+
+// Run the suite itself w/ normal go test function
+func TestLinkedListTestSuite(t *testing.T) {
+	suite.Run(t, new(LinkedListTestSuite))
+}
