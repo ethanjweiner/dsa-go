58f1b358d28c47632bf3d28cb89e2a61d7b422cd
Ethan Weiner <ethanweiner20@gmail.com>
2022-12-15 15:56:54 -0500

    feat(queue): add helpers

--- structures/queue.go
+++ structures/queue.go
@@ -1,19 +1,27 @@
 package structures
 
-type Queue struct {
-	items []any
+type Queue[T any] struct {
+	items []T
 }
 
-func (q *Queue) Enqueue(item any) {
+func (q *Queue[T]) Enqueue(item T) {
 	q.items = append(q.items, item)
 }
 
-func (q *Queue) Dequeue() (dequeued any) {
+func (q *Queue[T]) Dequeue() (dequeued T) {
 	if len(q.items) == 0 {
-		return nil
+		return
 	}
 
 	dequeued = q.items[0]
 	q.items = q.items[1:]
 	return
+}
+
+func (q *Queue[T]) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+func (q *Queue[T]) Length() int {
+	return len(q.items)
 }
